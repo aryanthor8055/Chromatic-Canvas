1160ecf402f76ef047981d0a71f4037de5fa59ba
Aryan <aryan16072001236809@gmail.com>
2025-10-25 14:55:51 +0530

    Created Stats and Features Section,Add Intersection observer for feature cards

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,38 @@
+import FeaturesSection from "@/components/features";
 import HeroSection from "@/components/hero";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
+  const stats=[
+    {label:"Images Processed",value:10000,suffix:"+"},
+    {label:"Active Users",value:500,suffix:"+"},
+    {label:"AI Transformations",value:45000,suffix:"+"},
+    {label:"User Satisfaction",value:98,suffix:"%"},
+  ]
   return (
    <div className="pt-36">
     {/* Hero */}
     <HeroSection/>
     {/* Stats */}
+    <section className="py-20">
+      <div className="max-w-6xl mx-auto px-6">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
+
+        {stats.map((stat,index)=>{
+          return <div key={index} className="text-center">
+            <div className="text-4xl lg:text-5xl font-bold mb-2 bg-gradient-to-r from-cyan-400 to-blue-500 bg-clip-text text-transparent">
+              {stat.value.toLocaleString()}
+              {stat.suffix}
+            </div>
+            <div className="text-gray-400 uppercase tracking-wider text-sm">{stat.label}</div>
+          </div>
+        })}
+        </div>
+      </div>
+    </section>
     {/* features */}
+    <FeaturesSection/>
     {/* pricing */}
     <section className="py-20 text-center">
       <div className="max-w-4xl mx-auto px-6">
@@ -18,7 +42,7 @@ export default function Home() {
       </div>
       <Link href='/dashboard'>
       <Button variant="primary" size="xl">
-      💥Start Creating Now
+      💥 Start Creating Now
       </Button>
       </Link>
     </section>
---
+++ hooks/use-intersection-observer.jsx
@@ -0,0 +1,18 @@
+'use client'
+import { useEffect, useRef, useState } from "react"
+
+
+const useInterfaceObserver = (threshold=0.1) => {
+    const [isVisible,setIsVisible]=useState(false)
+    const ref=useRef()
+    useEffect(()=>{
+       const observer= new IntersectionObserver(([entry])=>setIsVisible(entry.isIntersecting),{threshold})
+
+       if(ref.current) observer.observe(ref.current)
+        return()=>observer.disconnect()
+    },[threshold])
+  
+    return [ref,isVisible]
+}
+
+export default useInterfaceObserver
