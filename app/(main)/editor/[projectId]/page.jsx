da88d5685bf3dab370e59c11c0bb2bfad95c2b8e
Aryan <aryan16072001236809@gmail.com>
2025-10-28 12:41:50 +0530

    Setup getProjectbyId and updateProject Handler and setup context

--- app/(main)/editor/[projectId]/page.jsx
+++ app/(main)/editor/[projectId]/page.jsx
@@ -1,12 +1,45 @@
-'use client'
-import { useParams } from 'next/navigation'
-import React from 'react'
+"use client";
+import { CanvasContext } from "@/context/context";
+import { Monitor } from "lucide-react";
+import { useParams } from "next/navigation";
+import React, { useState } from "react";
 
-const Editor = async() => {
-    const {projectId}=await useParams()
+const Editor = () => {
+  const params = useParams();
+  const projectId = params.projectId;
+
+  const [canvasEditor, setCanvasEditor] = useState(null);
+  const [processingMessage, setProcessingMessage] = useState(null);
+
+  const [activeTool, setActiveTool] = useState("resize");
   return (
-    <div>Editor:{projectId}</div>
-  )
-}
+    <CanvasContext.Provider
+      value={{
+        canvasEditor,
+        setCanvasEditor,
+        activeTool,
+        onToolChange: setActiveTool,
+        processingMessage,
+        setProcessingMessage,
+      }}
+    >
 
-export default Editor
+      <div className="lg:hidden min-h-screen bg-slate-900 flex items-center justify-center p-6">
+        <div className="text-center mx-w-md">
+          <Monitor className="h-16 w-16 text-cyan-400 mx-auto mb-6"/>
+          <h1 className="text-2xl font-bold text-white mb-4">
+            Desktop Required
+          </h1>
+          <p className="text-white/50 text-sm">
+          Please use a larger screen to access the full editing experience
+          </p>
+        </div>
+      </div>
+      <div className="hidden lg:block min-h-screen bg-slate-900">
+        Editor :{projectId}
+      </div>
+    </CanvasContext.Provider>
+  );
+};
+
+export default Editor;
--- convex/projects.js
+++ convex/projects.js
@@ -83,3 +83,75 @@ export const deleteProject=mutation({
     }
 })
 
+export const getProject=query({
+    args:{projectId:v.id("projects")},
+
+     handler:async(ctx,args)=>{
+        const user=await ctx.runQuery(internal.users.getCurrentUser)
+
+       const project=await ctx.db.get(args.projectId)
+
+       if(!project){
+        throw new Error("Project not found")
+       }
+
+       if(!user || project.userId!==user._id){
+        throw new Error("Access denied")
+       }
+       return project;
+
+    }
+
+})
+
+export const updateProject=mutation({
+    args:{
+    projectId: v.id("projects"),
+    canvasState: v.optional(v.any()),
+    width: v.optional(v.number()), 
+    height: v.optional(v.number()),
+    currentImageUrl: v.optional(v.string()),
+    thumbnailUrl: v.optional(v.string()),
+    activeTransformations: v.optional(v.string()),
+    backgroundRemoved: v.optional(v.boolean()),
+    },
+
+    handler:async()=>{
+          const user=await ctx.runQuery(internal.users.getCurrentUser)
+
+       const project=await ctx.db.get(args.projectId)
+
+       if(!project){
+        throw new Error("Project not found")
+       }
+
+        if(!user || project.userId!==user._id){
+        throw new Error("Access denied")
+       }
+
+       const updatedData={
+        updatedAt:Date.now()
+       }
+
+       if(args.canvasState!==undefined)
+        updatedData.canvasState=args.canvasState;
+    if(args.width!==undefined)
+        updatedData.width=args.width;
+    if(args.height!==undefined)
+        updatedData.height=args.height;
+    if(args.currentImageUrl!==undefined)
+        updatedData.currentImageUrl=args.currentImageUrl;
+    if(args.thumbnailUrl!==undefined)
+        updatedData.thumbnailUrl=args.thumbnailUrl;
+    if(args.activeTransformations!==undefined)
+        updatedData.activeTransformations=args.activeTransformations;
+     if(args.backgroundRemoved!==undefined)
+        updatedData.backgroundRemoved=args.backgroundRemoved;
+
+     await ctx.db.patch(args.projectId,updatedData)
+
+    return args.projectId
+    
+    }
+})
+
